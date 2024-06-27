6d0c8d13f46bac3ca025b7625b7091bca1b9b12f
caijjie <caijjie321@gmail.com>
2024-06-27 15:43:42 +0800

    创建新项目，设计MainActivity基本页面等

---
+++ app/src/main/java/com/sagereal/factorymode/MainActivity.java
@@ -0,0 +1,39 @@
+package com.sagereal.factorymode;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        findViewById(R.id.btn_single_test).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, SingleTestActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
---
+++ app/src/main/java/com/sagereal/factorymode/SingleTestActivity.java
@@ -0,0 +1,15 @@
+package com.sagereal.factorymode;
+
+import android.os.Bundle;
+import android.text.NoCopySpan;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SingleTestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_test);
+    }
+}
