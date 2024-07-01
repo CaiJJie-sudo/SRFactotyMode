02dcc7e11317754eeb19861b1b342df3d33e7e7f
caijjie <caijjie321@gmail.com>
2024-07-01 20:08:03 +0800

    新增单项测试部分功能

--- app/src/main/java/com/sagereal/factorymode/SingleTestActivity.java
+++ app/src/main/java/com/sagereal/factorymode/SingleTestActivity.java
@@ -6,12 +6,21 @@ import android.view.View;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sagereal.factorymode.databinding.ActivityMainBinding;
+import com.sagereal.factorymode.databinding.ActivitySingleTestBinding;
 
 public class SingleTestActivity extends AppCompatActivity implements View.OnClickListener {
+    private ActivitySingleTestBinding binding;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_single_test);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_single_test);
+        // 显示系统导航键
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        RecyclerView rvSingleTest = binding.rvSingleTest;
         findViewById(R.id.ib_back).setOnClickListener(this);
     }
 
